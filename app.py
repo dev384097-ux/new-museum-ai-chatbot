70c77b8d0afcb14ff04c29066eef4786a2994d5e
Dev <dvarshney266@gmail.com>
2026-04-14 18:22:39 +0530

    fix: sanitize Mail credentials for whitespace

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', '').strip()
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', '').strip()
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME', '').strip()
 app.config['MAIL_DEBUG'] = True
 
 mail = Mail(app)
